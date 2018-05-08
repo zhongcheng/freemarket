28536b3a118a2016de74a7ab9561f089ece882bb
zhongcheng <Dick19900728!>
2018-05-08 21:41:01 +0200

    Modify listing\urls.py comments

--- listing/urls.py
+++ listing/urls.py
@@ -4,34 +4,34 @@ from . import views
 app_name = 'listing'
 
 urlpatterns = [
-    # /listing/
+    # /
     url(r'^$', views.index, name='index'),
 
-    # /listing/
+    # /visitor/
     url(r'^visitor/$', views.index_visitor, name='index_visitor'),
 
-    # /listing/register/
+    # /register/
     url(r'^register/$', views.register, name='register'),
 
-    # /listing/login_user/
+    # /login_user/
     url(r'^login_user/$', views.login_user, name='login_user'),
 
-    # /listing/logout_user/
+    # /logout_user/
     url(r'^logout_user/$', views.logout_user, name='logout_user'),
 
-    # /listing/<city_id>/
+    # /<item_id>/detail/
     url(r'^(?P<item_id>[0-9]+)/detail/$', views.detail, name='detail'),
 
-    # /listing/item/add/
+    # /add_item/
     url(r'^add_item/$', views.add_item, name='add_item'),
 
-    # /listing/
+    # /my_item/
     url(r'^my_items/$', views.my_items, name='my_items'),
 
-    # /listing/item/<city_id>/
+    # /<item_id>/update_item/
     url(r'^(?P<item_id>[0-9]+)/update_item/$', views.update_item, name='update_item'),
 
-    # /listing/item/<city_id>/delete/
+    # /<item_id>/delete_item/
     url(r'^(?P<item_id>[0-9]+)/delete_item/$', views.delete_item, name='delete_item'),
 
 ]
