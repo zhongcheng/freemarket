c1b6dc582325bbe0baa9d0a330c075949f00c779
zhongcheng <Dick19900728!>
2018-05-16 22:03:13 +0200

    remove captcha on login page

--- listing/views.py
+++ listing/views.py
@@ -161,20 +161,19 @@ def logout_user(request):
 
 
 def login_user(request):
-    if request.recaptcha_is_valid:
-        if request.method == "POST":
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    items = Item.objects.all().order_by('-id')[:100]
-                    return render(request, 'listing/index.html', {'items': items})
-                else:
-                    return render(request, 'listing/login.html', {'error_message': 'Your account has been disabled'})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                items = Item.objects.all().order_by('-id')[:100]
+                return render(request, 'listing/index.html', {'items': items})
             else:
-                return render(request, 'listing/login.html', {'error_message': 'Invalid login'})
+                return render(request, 'listing/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'listing/login.html', {'error_message': 'Invalid login'})
     return render(request, 'listing/login.html')
 
 
