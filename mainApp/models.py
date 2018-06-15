cb512674754be3c111f708c88e4530b02f97890f
zhongcheng <Dick19900728!>
2018-06-15 19:43:00 +0200

    Revert "auto rotate photo uploaded by phone"
    This reverts commit fc2e439c0e778f51c6ebdb326b36717e3986d346.

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,12 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-from PIL import Image, ExifTags
+from PIL import Image
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
-import os
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 
 # do not forget to update database (migrate) after changing the structure of a class
@@ -33,29 +30,4 @@ class Item(models.Model):
 
     def __str__(self):
         return self.item_name
-
-
-@receiver(post_save, sender=Item, dispatch_uid="update_item_photo")
-def auto_rotate_photo(sender, instance, **kwargs):
-    if instance.photo:
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        image_path = BASE_DIR + instance.photo.url
-        try:
-            image = Image.open(image_path)
-            for orientation in ExifTags.TAGS.keys():
-                if ExifTags.TAGS[orientation] == 'Orientation':
-                    break
-            exif = dict(image._getexif().items())
-
-            if exif[orientation] == 3:
-                image = image.rotate(180, expand=True)
-            elif exif[orientation] == 6:
-                image = image.rotate(270, expand=True)
-            elif exif[orientation] == 8:
-                image = image.rotate(90, expand=True)
-            image.save(image_path)
-            image.close()
-        except (AttributeError, KeyError, IndexError):
-            # cases: image don't have getexif
-            pass
 
