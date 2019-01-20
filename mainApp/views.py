3118e86d3031345755cfacd1c77ee89c191e2f88
zhongcheng <Dick19900728!>
2019-01-20 18:52:10 +0100

    change terms page template to kate

--- mainApp/views.py
+++ mainApp/views.py
@@ -152,7 +152,7 @@ def terms(request):
     if request.user.is_authenticated:
         base_template_name = 'mainApp/base.html'
     else:
-        base_template_name = 'mainApp/base_visitor.html'
+        base_template_name = 'mainApp/base_visitor_kate.html'
     return render(request, 'mainApp/terms.html', {'base_template_name': base_template_name})
 
 
