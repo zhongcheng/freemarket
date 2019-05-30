d707710b0e959b02c550da6644dd5ee6004e2bf1
zhongcheng <Dick19900728!>
2019-05-30 12:29:36 +0200

    fixes for url styling

--- mainApp/templatetags/url_target_blank.py
+++ mainApp/templatetags/url_target_blank.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 
 def url_target_blank(text):
-    return text.replace('<a ', '<a target="_blank" ')
+    return text.replace('<a ', '<a target="_blank" style="text-decoration: none" ')
 
 url_target_blank = register.filter(url_target_blank, is_safe=True)
